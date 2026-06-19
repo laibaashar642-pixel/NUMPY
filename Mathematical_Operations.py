a4ede53dbe01f43bc3efcadf6092d8b8ac44132b
Laiba Ashar <asharmunir642@gmail.com>
2026-06-19 11:10:43 +0500

    Mathematical Operations

--- Mathematical_Operations.py
+++ Mathematical_Operations.py
@@ -1,3 +1,15 @@
-#We can perform easily fast operations on arrays
-
-
+#We can perform easily fast operations on arrays +,-*,/
+import numpy as np
+arr=np.array([32,89,90,12])
+print(arr+5)
+print(arr*4)
+print(arr**4)
+print(arr/4)
+#Aggregation function means summarizing the actual data
+print(np.sum(arr))
+print(np.average(arr))
+print(np.mean(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.std(arr))
+print(np.var(arr))
