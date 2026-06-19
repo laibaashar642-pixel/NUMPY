ed54eb07e31f11b925ac13051218807053a59973
Laiba Ashar <asharmunir642@gmail.com>
2026-06-19 16:50:03 +0500

    Broadcasting & Vectorization

--- Advanced_Numpy/Inserting_operations.py
+++ Advanced_Numpy/Inserting_operations.py
@@ -22,3 +22,26 @@ arr1=np.array([23,89,90])
 arr2=np.array([20,69,9])
 newarr=np.concatenate((arr1,arr2))
 print(newarr) #concatenate function hamaray pass elemnets kai end pr add kry gha jb kai jo hamary pass insert function hai wo specific point pr element add kry gah
+#Deleting Commmand np.delete(array,index,axis=None)Flattern Array It is a method of deleting oned array
+arr=np.array([34,57,90,12])
+print(arr)
+newarr=np.delete(arr,0)
+print(newarr)
+#It is a method of deleting 2d array
+arr_2d=np.array([[1,2,3],[4,5,6]])
+newarr_2d=np.delete(arr_2d,0,axis=0)
+print(newarr_2d)
+#Stacking means from top to bottom vertically horizontally vstack() rowwise  hstack() columnwise
+arr1=np.array([89,67,34,12])
+arr2=np.array([23,90,12,34])
+print(np.vstack((arr1,arr2)))#Vertically Stack
+print(np.hstack((arr1,arr2)))#Horizontally Stack
+#Splitting np.split() equal np.hsplit(),np.vsplit()
+arr=np.array([12,34,56,78])
+print(np.split(arr,2))
+# Broadcasting ka matlab hai different sizes ke arrays par arithmetic operations perform karna bina manually loops likhe.Vectorization ka matlab hai operations ko poore array par ek saath perform karna instead of using loops.
+prices=[100,200,400]
+discount=10 #10%discount
+final_prices=[]
+for price in prices:
+    final_prices = price - (prices * discount/100)
